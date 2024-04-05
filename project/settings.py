58bd291749f3b02e681a552a1c103fd3c192a8d1
sara-mohamd <saraamohamed0038@gmail.com>
2024-04-05 03:59:23 +0200

    this is user app including its models

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
