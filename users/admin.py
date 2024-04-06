14851117199c4cfe17fec0751d9abac882b147c5
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-06 04:36:16 +0200

    Login and Sign up app with basic design 1

---
+++ users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Users, UserRecipe
+
+
+admin.site.register(Users)
+admin.site.register(UserRecipe)
+
+# Register your models here.
---
+++ users/views.py
@@ -0,0 +1,7 @@
+
+from django.shortcuts import render
+
+
+# this is dummy function only to check if it works or not.
+def dummy(request):
+    return render(request, 'base.html')
