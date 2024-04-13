4365eb73211a9cde96b1d2aabeee9db31357fb87
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-14 01:59:41 +0200

    conflicted fixed and favorite app updated

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import add_to_favorite, remove_from_favorite
 
-app_name = 'favorites'
-
 urlpatterns = [
-    path('add/', add_to_favorite, name='add_to_favorite'),
-    path('remove/', remove_from_favorite, name='remove_from_favorite'),
+    path('add/<int:recipe_id>/', add_to_favorite, name='add_to_favorite'),
+    path('remove/<int:recipe_id>/', remove_from_favorite, name='remove_from_favorite'),
 ]
