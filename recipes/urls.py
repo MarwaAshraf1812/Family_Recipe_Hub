3dd98821cdd27f6a14824d462c46a5a6addb7b21
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-15 22:46:03 +0200

    recipe updated

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.recipes, name='recipes'),
 
-    path('recipe', views.recipe, name='recipe')
+    path('recipe/', views.recipe, name='recipe')
 
 ]
