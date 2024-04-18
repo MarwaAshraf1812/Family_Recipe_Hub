096923e24f0657410b3c770ee993bc79ec459cb2
marwa ashraf <126331254+MarwaAshraf1812@users.noreply.github.com>
2024-04-18 18:16:00 +0200

    Update forms.py

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,13 +1,6 @@
-# forms.py in your recipes app
+# forms.py
+
 from django import forms
-from .models import Recipe, Ingredient, Instruction, RecipeImage
-
-
-class RecipeForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = '__all__'  # Use all fields from the Recipe model
-
 
 class RecipeSearchForm(forms.Form):
-    SearchRequest = forms.CharField(initial='Search Recipe', max_length=100)
+    search_query = forms.CharField(label='Search')
